4497678c517bbeda7e0cb2119d2852748ffb9c8c
HL <kmr345@naver.com>
2021-03-30 17:48:50 +0900

    2021_03_30

---
+++ java/Ex/src/Ex15.java
@@ -0,0 +1,16 @@
+
+public class Ex15 {
+	public static void main(String[] args)
+	{
+		int a = 1;
+		int b = 2;
+		int temp;
+		
+		temp = a;
+		a = b;
+		b = temp;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
---
+++ java/Test/src/Hi.java
@@ -0,0 +1,13 @@
+///**
+// * 
+// */
+//
+///**
+// * @author HL
+// *
+// */
+//public class Hi {
+//	public static void main(String[] args) {
+//		System.out.println("Hi~ Java");
+//	}
+//}
