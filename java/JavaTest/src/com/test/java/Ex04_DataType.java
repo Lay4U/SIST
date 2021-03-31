b29c731df593fd3642fad33512f45a6e78a62462
HL <kmr345@naver.com>
2021-03-31 20:03:44 +0900

    2021_03_31

---
+++ java/JavaTest/src/com/test/java/Ex04_DataType.java
@@ -0,0 +1,7 @@
+package com.test.java;
+
+public class Ex04_DataType {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ java/JavaTest/src/com/test/java/Ex07_DataType.java
@@ -0,0 +1,7 @@
+package com.test.java;
+
+public class Ex07_DataType {
+	public static void main(String[] args) {
+		
+	}
+}
