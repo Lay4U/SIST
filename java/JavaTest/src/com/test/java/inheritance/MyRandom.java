ae2526259a45b24b411bffc3c14bfc07960c1094
HL <kmr345@naver.com>
2021-04-21 17:56:16 +0900

    2021_04_21

---
+++ java/JavaTest/src/com/test/java/inheritance/MyRandom.java
@@ -0,0 +1,50 @@
+package com.test.question.inheritance;
+
+import java.util.Random;
+
+public class MyRandom {
+	
+	public int nextInt() { //구색 갖추기용
+		
+		Random rnd = new Random();
+		
+		return rnd.nextInt();
+		
+	}
+	
+	public int nextTinyInt() {
+		
+		Random rnd = new Random();
+		
+		return rnd.nextInt(10) + 1;		
+	}
+	
+	public String nextColor() {
+		
+		Random rnd = new Random();
+		
+		String[] color = { "red", "yellow", "blue", "orange", "green" };
+		
+		return color[rnd.nextInt(color.length)];
+		
+	}
+	
+	public boolean nextBoolean() { //구색 갖추기
+		
+		Random rnd = new Random();
+		
+		return rnd.nextBoolean();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
