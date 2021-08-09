cec7a07626a274d8b6d923c4e948b6473a5ae674
HL <kmr345@naver.com>
2021-08-10 07:30:11 +0900

    Revert "end jsp project"
    This reverts commit 4dd11044090a12701cb16ea91d17682de8eb7792 [formerly 7aa47118c265d2a2fbd3f4f75d0ee64a2a16c820].
    
    
    Former-commit-id: 46eece7914d05d748dc574ac9d51b7c2dff9f157

--- server/myapp/src/main/java/com/test/cookie/aa.java
+++
@@ -1,5 +0,0 @@
-package com.test.cookie;
-
-public class aa {
-
-}
--- server/myapp/src/main/java/com/test/myapp/board/Template.java
+++ server/myapp/src/main/java/com/test/myapp/board/Template.java
@@ -15,6 +15,8 @@ public class Template extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		
+
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/board/template.jsp");
 		dispatcher.forward(req, resp);
 	}
